cdd09eb6a862226e438e0afbf8d3da4c79535261
nickreynolds <nicholas.s.reynolds@gmail.com>
2021-11-23 19:24:28 -0500

    first commit

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.d.ts",
+    "!**/node_modules/**",
+  ],
+  coverageReporters: ["text", "lcov", "json"],
+  roots: ["<rootDir>/src"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  testEnvironment: "node",
+  testTimeout: 20000,
+};
